482e200a52af53cba465b888e4219421a89e0ab0
Smit soni <smith.s@azguardstech.com>
2025-11-07 18:31:29 +0530

    fix cors issue

--- src/main/java/dev/sultanov/keycloak/multitenancy/resource/MultitenancyRootResource.java
+++ src/main/java/dev/sultanov/keycloak/multitenancy/resource/MultitenancyRootResource.java
@@ -1,27 +1,39 @@
 package dev.sultanov.keycloak.multitenancy.resource;
 
+import org.keycloak.http.HttpRequest;
 import org.keycloak.models.KeycloakSession;
 
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 @Path("/")
 public class MultitenancyRootResource {
 
-    private final KeycloakSession session;
+	private final KeycloakSession session;
 
-    public MultitenancyRootResource(KeycloakSession session) {
-        this.session = session;
-    }
+	public MultitenancyRootResource(KeycloakSession session) {
+		this.session = session;
+	}
 
-    @Path("switch")
-    public Object switchTenant() {
-        return new SwitchActiveTenant(session);
-    }
+	@Path("{any:.*}")
+	public Object handleAll() {
+		HttpRequest request = session.getContext().getHttpRequest();
 
-    @Path("user-tenants")
-    public Object userTenants() {
-        return new GetUserTenants(session);
-    }
+		if (request != null && "OPTIONS".equalsIgnoreCase(request.getHttpMethod())) {
+			return new CorsResource();
+		}
 
-    // You can keep expanding this with other tenant sub-resources
+		// Manually route specific endpoints
+		String path = session.getContext().getUri().getPath();
+
+		if (path.endsWith("/switch")) {
+			return new SwitchActiveTenant(session);
+		} else if (path.endsWith("/user-tenants")) {
+			return new GetUserTenants(session);
+		}
+
+		return Response.status(Response.Status.NOT_FOUND).build();
+	}
+
+	// You can keep expanding this with other tenant sub-resources
 }
